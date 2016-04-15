18cfdfd7a5f596e74a6ab28c17fa14fb0f58851f
Christoph Eickhoff <Christoph.Eickhoff@cs.uni-kassel.de>
2016-04-15 13:28:16 +0200

    add complex metric

---
+++ src/de/unikassel/ttc2016/Metric.java
@@ -0,0 +1,21 @@
+package de.unikassel.ttc2016;
+
+import de.unikassel.ttc2016.model.ClassModel;
+
+public class Metric
+{
+   /**
+    * @param classModel
+    * @return heavy calculated things
+    */
+   public double calc(ClassModel classModel)
+   {
+
+      if (classModel.getClasses().size() >= 42)
+      {
+         return 0;
+      }
+
+      return classModel.getClasses().size() / 42;
+   }
+}
